d5bc6db16cd0fcdc44e5131f69709983be709339
Alonso <alonso.decosio@dynatrace.com>
2018-08-10 16:56:20 -0500

    Form Validation

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -4,8 +4,11 @@ import classes from './Button.css';
 
 const button = (props) => (
     <button
+        disabled={props.disabled}
         className={[classes.Button, classes[props.btnType]].join(' ')}
-        onClick={props.clicked}>{props.children}</button>
+        onClick={props.clicked}>
+            {props.children}
+    </button>
 );
 
 export default button;
